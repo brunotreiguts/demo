ce47310fbcb6040f34b6941236b6dad8c2cdd308
Bruno <bruno.treiguts@gmail.com>
2014-06-19 06:35:35 +0300

    Izmainiti linki, pievienota jauna sadaļu funkcionalitāte.

--- fuel/app/views/contact/indexlv.php
+++
@@ -1,8 +0,0 @@
-<h2><?php echo __('NAV_CONTACT') ?></h2>
-<div class="well">
-<iframe 
-    class="map" 
-    frameborder="0"
-    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2176.290303173858!2d24.080983700000008!3d56.94382739999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46eed02183778af5%3A0xb17f8ccbaa154c5b!2sMe%C5%BEa+iela+4!5e0!3m2!1slv!2s!4v1401750838692" >
-</iframe>
-</div>
--- fuel/app/views/massage/indexen.php
+++
@@ -1,1 +0,0 @@
-TEKSTS ANGĻU VALODĀ.
